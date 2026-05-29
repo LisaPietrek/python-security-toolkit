4fd5345199de1f0b136abc77ef8bb273b2b808bd
Lisa Pietrek <lipietre@biophys.mpg.de>
2026-05-29 16:47:07 +0200

    Add exception handling

--- port_scanner.py
+++ port_scanner.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 # use scanme nmap as a target for testing
 # use the URL as input for the target website, you want to check
-#URL = sys.argv[1]
 URL = input("Enter target URL: ")
 
 # try to get the IP for the website to avoid the addition of the overhead DNS lookup - may slow down the initial connection
@@ -23,18 +22,29 @@ print("Scanning target: " + target)
 print("Scan started at: " + str(datetime.now()))
 print("-" * 50)
 
+# handle exceptions
+try:
+    # loop port range for checking
+    for port in range(1, 1500):
+        # initialize socket object with connection-oriented TCP
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+    
+        result = s.connect_ex((target, port))
+    
+        if result == 0:
+            print(f"Port {port} is OPEN")
+        else:
+            print(f"Port {port} is CLOSED")
+    
+        s.close()
 
-# loop port range for checking
-for port in range(1, 1500):
-    # initialize socket object with connection-oriented TCP
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(1)
-
-    result = s.connect_ex((target, port))
-
-    if result == 0:
-        print(f"Port {port} is OPEN")
-    else:
-        print(f"Port {port} is CLOSED")
-
-    s.close()
+except KeyboardInterrupt:
+    print("\nExiting Program.")
+    sys.exit()
+except s.gaierror:
+   print("\nHostname could not resolved.")
+   sys.exit()
+except s.error:
+   print("\nServer not responding.")
+   sys.exit()	
