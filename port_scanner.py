a07f4190f2b760bb1fcabda6d27291a87a489ebf
Lisa Pietrek <lipietre@biophys.mpg.de>
2026-05-29 16:31:35 +0200

    Target as input, scan broader port range

--- port_scanner.py
+++ port_scanner.py
@@ -1,13 +1,31 @@
 import socket
+import sys
+from datetime import datetime
 
 # use scanme nmap as a target for testing
-target = "scanme.nmap.org"
+# use the URL as input for the target website, you want to check
+#URL = sys.argv[1]
+URL = input("Enter target URL: ")
 
-# use three example port nr to test first
-ports = [22, 80, 443]
+# try to get the IP for the website to avoid the addition of the overhead DNS lookup - may slow down the initial connection
+try: 
+    target = socket.gethostbyname(URL) 
+except socket.gaierror: 
 
-# loop ports for checking
-for port in ports:
+    # this means could not resolve the host 
+    print ("there was an error resolving the host")
+    # stop here in case of a problem
+    sys.exit() 
+
+# make output a bit more informative
+print("-" * 50)
+print("Scanning target: " + target)
+print("Scan started at: " + str(datetime.now()))
+print("-" * 50)
+
+
+# loop port range for checking
+for port in range(1, 1500):
     # initialize socket object with connection-oriented TCP
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
